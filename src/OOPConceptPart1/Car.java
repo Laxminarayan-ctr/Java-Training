6698ab70debc21288d75a1aee3cce16d950a8078
laxminarayan.sharma <laxminarayan.sharma@hapana.com>
2025-09-09 14:31:01 +0530

    new changes

--- src/OOPConceptPart1/Car.java
+++ src/OOPConceptPart1/Car.java
@@ -1,30 +1,28 @@
 package OOPConceptPart1;
 
-public class Car {
-	
-	//Class variable
+
+public class Car
+{
 	
 	int mod;
 	int wheel;
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+	
+	public static void main(String[] args)
+	{
 		Car a = new Car();
 		Car b = new Car();
 		Car c = new Car();
 		
-		a.mod = 2012;
-		a.wheel= 4;
+		a.mod = 21;
+		a.wheel = 12;
+		b.mod = 1029;
+		b.wheel = 933;
+		System.out.println(a.mod);
+		System.out.println(b.wheel);
 		
-		b.mod = 2013;
-		b.wheel = 4;
 		
-		c.mod = 2015;
-		c.wheel = 4;
-		
-		System.out.println(c.mod);
-
 	}
-
+	
+	
+	
 }
